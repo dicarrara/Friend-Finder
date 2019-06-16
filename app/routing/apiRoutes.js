7410026fc9ff16fa3bb655a8fefddcfa57b8fdd1
Diana Carrara <dianaVcarrara@gmail.com>
2019-06-16 14:08:56 -0600

    added parsInt in script, so we recieve correct value for future use

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -11,9 +11,32 @@ module.exports = function(app) {
     var newFriend = req.body;
     // compute best match from scores
     var bestMatch = {};
-    
-    console.log(newFriend);
+
+    // ------ compare difference
+    var bestMatchIndex = 0;
+    var greatestDifference = 40;
+
+    for (var i = 0; i < friends.length; i++) {
+      var totalDifference = 0;
+      for (var j = 0; j < friends[i].scores.length; j++) {
+        var differenceInScores = Math.abs(
+          friends[i].scores[j] - newFriend.scores[j]
+        );
+        totalDifference += differenceInScores;
+        console.log("difference in score", differenceInScores);
+        console.log("Total dif", totalDifference);
+      }
+      // if the totalDifference in scores is less than the best match so far save that index and difference
+      if (totalDifference < greatestDifference) {
+        bestMatchIndex = i;
+        greatestDifference = totalDifference;
+      }
+    }
+
+    // the best match index is used to get the best match data from the friends index
+    bestMatch = friends[bestMatchIndex];
+
     friends.push(newFriend);
-    res.json(newFriend);
+    res.json(bestMatch);
   });
 };
